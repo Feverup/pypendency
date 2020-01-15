977f06034c8596240eee9815e12c6216528b9f98
Luis Pintado Lozano <luis.pintado@feverup.com>
2020-01-15 09:36:25 +0100

    Add load_dir to loader interface

--- src/pypendency/loaders/loader.py
+++ src/pypendency/loaders/loader.py
@@ -4,3 +4,6 @@ from abc import ABC, abstractmethod
 class Loader(ABC):
     @abstractmethod
     def load(self, resource: str) -> None: pass
+
+    @abstractmethod
+    def load_dir(self, directory: str) -> None: pass
