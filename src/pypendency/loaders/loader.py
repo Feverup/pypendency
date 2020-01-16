b1879c0db90b65b0e87c05ea7e07eae11b4bb522
Luis Pintado Lozano <luis.pintado@feverup.com>
2020-01-16 13:10:03 +0100

    Add guard method to the loader ABC

--- src/pypendency/loaders/loader.py
+++ src/pypendency/loaders/loader.py
@@ -1,4 +1,7 @@
+import os
 from abc import ABC, abstractmethod
+
+from pypendency.loaders.exceptions import PathNotAbsolute
 
 
 class Loader(ABC):
@@ -7,3 +10,7 @@ class Loader(ABC):
 
     @abstractmethod
     def load_dir(self, directory: str) -> None: pass
+
+    def guard_path_is_absolute(self, path: str):
+        if not os.path.isabs(path):
+            raise PathNotAbsolute(path)
