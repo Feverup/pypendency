648e6552fc69c87ab8dc4d1ca687e3a44e1e5bd4
Luis Pintado Lozano <luis.pintado@feverup.com>
2020-01-16 12:38:07 +0100

    Add load method by path and renamed existing one to be by_module_name

--- src/pypendency/python_loadable_module_type.py
+++ src/pypendency/python_loadable_module_type.py
@@ -4,4 +4,4 @@ from pypendency.builder import ContainerBuilder
 
 
 class PythonLoadableModuleType(ModuleType):
-    def load(self, container_builder: ContainerBuilder): ...
+    def load(self, container_builder: ContainerBuilder) -> None: pass
