8154244fad897ac8e9487730e8b331c8a8c04484
Luis Pintado Lozano <luis.pintado@feverup.com>
2020-01-16 13:11:05 +0100

    Add tests

--- tests/loaders/test_py_loader.py
+++ tests/loaders/test_py_loader.py
@@ -1,5 +1,5 @@
 import os
-from unittest import TestCase, skip
+from unittest import TestCase
 
 from pypendency.builder import ContainerBuilder
 from pypendency.loaders import exceptions
@@ -13,20 +13,23 @@ class TestPyLoader(TestCase):
     def setUp(self) -> None:
         self._container_builder = ContainerBuilder([])
         self.loader = PyLoader(self._container_builder)
+        self.current_dir = os.path.dirname(__file__)
+
+    def test_load_fails_for_non_absolute_path(self):
+        with self.assertRaises(exceptions.PathNotAbsolute):
+            self.loader.load("relative/path")
 
     def test_load_fails_for_non_existing_file(self):
         with self.assertRaises(exceptions.ResourceNotFound):
             self.loader.load("/non/existing/path")
 
     def test_load_fails_for_loader_without_load_method(self):
-        current_dir_path = os.path.dirname(__file__)
-        path = os.path.join(current_dir_path, "..", "resources", "test_di_no_load_method.py")
+        path = os.path.join(self.current_dir, "..", "resources", "test_di_no_load_method.py")
         with self.assertRaises(exceptions.MissingLoaderMethod):
             self.loader.load(path)
 
     def test_load_works_as_expected(self):
-        current_dir_path = os.path.dirname(__file__)
-        path = os.path.join(current_dir_path, "..", "resources", "test_di.py")
+        path = os.path.join(self.current_dir, "..", "resources", "test_di.py")
         self.loader.load(path)
         c = self._container_builder.get("example.C")
         self.assertIsInstance(c, C)
@@ -49,7 +52,12 @@ class TestPyLoader(TestCase):
         self.assertIsInstance(c.a, A)
 
     def test_load_dir(self):
-        self.loader.load_dir("tests/resources/loaders")
+        path = os.path.join(self.current_dir, "..", "resources", "loaders")
+        self.loader.load_dir(path)
         self.assertIsInstance(self._container_builder.get("same_level_file"), A)
         self.assertIsInstance(self._container_builder.get("one_level_file"), A)
         self.assertIsInstance(self._container_builder.get("two_levels_file"), A)
+
+    def test_load_dir_fails_for_non_absolute_path(self):
+        with self.assertRaises(exceptions.PathNotAbsolute):
+            self.loader.load_dir("tests/resources/loaders")
